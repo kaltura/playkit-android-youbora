53295f433533504207043d0d203f0cf22c6a1ac7
Gilad Nadav <gilad.nadav@kaltura.com>
2018-07-02 11:37:37 +0300

    sync with dev before spepration

--- youboraplugin/src/main/java/com/kaltura/playkit/plugins/youbora/YouboraPlugin.java
+++ youboraplugin/src/main/java/com/kaltura/playkit/plugins/youbora/YouboraPlugin.java
@@ -22,7 +22,6 @@ import com.kaltura.playkit.PKMediaConfig;
 import com.kaltura.playkit.PKPlugin;
 import com.kaltura.playkit.Player;
 import com.kaltura.playkit.PlayerEvent;
-import com.kaltura.playkit.plugin.youbora.BuildConfig;
 import com.kaltura.playkit.utils.Consts;
 import com.npaw.youbora.youboralib.data.Options;
 
@@ -52,10 +51,10 @@ public class YouboraPlugin extends PKPlugin {
             return "Youbora";
         }
 
-        @Override
-        public String getVersion() {
-            return BuildConfig.VERSION_NAME;
-        }
+//        @Override
+//        public String getVersion() {
+//            return BuildConfig.VERSION_NAME;
+//        }
 
         @Override
         public PKPlugin newInstance() {
@@ -178,7 +177,10 @@ public class YouboraPlugin extends PKPlugin {
                 return ;
             }
 
-            Map<String, Object> opt  = YouboraConfig.updateMediaConfig(pluginConfig, key, value);
+            Map<String, Object> opt = YouboraConfig.updateMediaConfig(pluginConfig, key, value);
+            if (opt == null) {
+                return;
+            }
             pluginManager.setOptions(opt);
         }
     };
