0ed43e3195145bbe7037548954a33c22730eca2f
Gourav Saxena <gourav.saxena@kaltura.com>
2020-11-10 14:06:43 +0530

    - Removed Space

--- youboraplugin/src/main/java/com/kaltura/playkit/plugins/youbora/pluginconfig/YouboraConfig.java
+++ youboraplugin/src/main/java/com/kaltura/playkit/plugins/youbora/pluginconfig/YouboraConfig.java
@@ -170,7 +170,6 @@ public class YouboraConfig {
         this.fastDataConfig = fastDataConfig;
     }
 
-
     public Options getYouboraOptions() {
         Options youboraOptions =  new Options();
 
