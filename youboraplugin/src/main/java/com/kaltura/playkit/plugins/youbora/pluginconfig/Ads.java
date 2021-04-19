544ed3d8c9da0c73465c2fd80a84827e86eb08dc
Gilad Nadav <gilad.nadav@kaltura.com>
2021-04-19 11:50:39 +0300

    clean import

--- youboraplugin/src/main/java/com/kaltura/playkit/plugins/youbora/pluginconfig/Ads.java
+++ youboraplugin/src/main/java/com/kaltura/playkit/plugins/youbora/pluginconfig/Ads.java
@@ -3,7 +3,6 @@ package com.kaltura.playkit.plugins.youbora.pluginconfig;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Ads {
 
