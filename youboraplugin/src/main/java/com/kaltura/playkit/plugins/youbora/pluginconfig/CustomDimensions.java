1d06f69ea2db44172025873fa29dcb00133cd0b2
Gilad Nadav <gilad.nadav@kaltura.com>
2021-04-21 12:32:38 +0300

    FEC-11127 -  Align Config (#56)
    *  align to youbora Options model
    
    * populate values only if they are given
    
    * add missing configs
    
    * upgrade youbora 6.7.32

---
+++ youboraplugin/src/main/java/com/kaltura/playkit/plugins/youbora/pluginconfig/CustomDimensions.java
@@ -0,0 +1,206 @@
+package com.kaltura.playkit.plugins.youbora.pluginconfig;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CustomDimensions {
+    @SerializedName(value="customDimension1", alternate={"param1"})
+    private String customDimension1;
+    @SerializedName(value="customDimension2", alternate={"param2"})
+    private String customDimension2;
+    @SerializedName(value="customDimension3", alternate={"param3"})
+    private String customDimension3;
+    @SerializedName(value="customDimension4", alternate={"param4"})
+    private String customDimension4;
+    @SerializedName(value="customDimension5", alternate={"param5"})
+    private String customDimension5;
+    @SerializedName(value="customDimension6", alternate={"param6"})
+    private String customDimension6;
+    @SerializedName(value="customDimension7", alternate={"param7"})
+    private String customDimension7;
+    @SerializedName(value="customDimension8", alternate={"param8"})
+    private String customDimension8;
+    @SerializedName(value="customDimension9", alternate={"param9"})
+    private String customDimension9;
+    @SerializedName(value="customDimension10", alternate={"param10"})
+    private String customDimension10;
+    @SerializedName(value="customDimension11", alternate={"param11"})
+    private String customDimension11;
+    @SerializedName(value="customDimension12", alternate={"param12"})
+    private String customDimension12;
+    @SerializedName(value="customDimension13", alternate={"param13"})
+    private String customDimension13;
+    @SerializedName(value="customDimension14", alternate={"param14"})
+    private String customDimension14;
+    @SerializedName(value="customDimension15", alternate={"param15"})
+    private String customDimension15;
+    @SerializedName(value="customDimension16", alternate={"param16"})
+    private String customDimension16;
+    @SerializedName(value="customDimension17", alternate={"param17"})
+    private String customDimension17;
+    @SerializedName(value="customDimension18", alternate={"param18"})
+    private String customDimension18;
+    @SerializedName(value="customDimension19", alternate={"param19"})
+    private String customDimension19;
+    @SerializedName(value="customDimension20", alternate={"param20"})
+    private String customDimension20;
+
+    public String getCustomDimension1() {
+        return customDimension1;
+    }
+
+    public void setCustomDimension1(String customDimension1) {
+        this.customDimension1 = customDimension1;
+    }
+
+    public String getCustomDimension2() {
+        return customDimension2;
+    }
+
+    public void setCustomDimension2(String customDimension2) {
+        this.customDimension2 = customDimension2;
+    }
+
+    public String getCustomDimension3() {
+        return customDimension3;
+    }
+
+    public void setCustomDimension3(String customDimension3) {
+        this.customDimension3 = customDimension3;
+    }
+
+    public String getCustomDimension4() {
+        return customDimension4;
+    }
+
+    public void setCustomDimension4(String customDimension4) {
+        this.customDimension4 = customDimension4;
+    }
+
+    public String getCustomDimension5() {
+        return customDimension5;
+    }
+
+    public void setCustomDimension5(String customDimension5) {
+        this.customDimension5 = customDimension5;
+    }
+
+    public String getCustomDimension6() {
+        return customDimension6;
+    }
+
+    public void setCustomDimension6(String customDimension6) {
+        this.customDimension6 = customDimension6;
+    }
+
+    public String getCustomDimension7() {
+        return customDimension7;
+    }
+
+    public void setCustomDimension7(String customDimension7) {
+        this.customDimension7 = customDimension7;
+    }
+
+    public String getCustomDimension8() {
+        return customDimension8;
+    }
+
+    public void setCustomDimension8(String customDimension8) {
+        this.customDimension8 = customDimension8;
+    }
+
+    public String getCustomDimension9() {
+        return customDimension9;
+    }
+
+    public void setCustomDimension9(String customDimension9) {
+        this.customDimension9 = customDimension9;
+    }
+
+    public String getCustomDimension10() {
+        return customDimension10;
+    }
+
+    public void setCustomDimension10(String customDimension10) {
+        this.customDimension10 = customDimension10;
+    }
+
+    public String getCustomDimension11() {
+        return customDimension11;
+    }
+
+    public void setCustomDimension11(String customDimension11) {
+        this.customDimension11 = customDimension11;
+    }
+
+    public String getCustomDimension12() {
+        return customDimension12;
+    }
+
+    public void setCustomDimension12(String customDimension12) {
+        this.customDimension12 = customDimension12;
+    }
+
+    public String getCustomDimension13() {
+        return customDimension13;
+    }
+
+    public void setCustomDimension13(String customDimension13) {
+        this.customDimension13 = customDimension13;
+    }
+
+    public String getCustomDimension14() {
+        return customDimension14;
+    }
+
+    public void setCustomDimension14(String customDimension14) {
+        this.customDimension14 = customDimension14;
+    }
+
+    public String getCustomDimension15() {
+        return customDimension15;
+    }
+
+    public void setCustomDimension15(String customDimension15) {
+        this.customDimension15 = customDimension15;
+    }
+
+    public String getCustomDimension16() {
+        return customDimension16;
+    }
+
+    public void setCustomDimension16(String customDimension16) {
+        this.customDimension16 = customDimension16;
+    }
+
+    public String getCustomDimension17() {
+        return customDimension17;
+    }
+
+    public void setCustomDimension17(String customDimension17) {
+        this.customDimension17 = customDimension17;
+    }
+
+    public String getCustomDimension18() {
+        return customDimension18;
+    }
+
+    public void setCustomDimension18(String customDimension18) {
+        this.customDimension18 = customDimension18;
+    }
+
+    public String getCustomDimension19() {
+        return customDimension19;
+    }
+
+    public void setCustomDimension19(String customDimension19) {
+        this.customDimension19 = customDimension19;
+    }
+
+    public String getCustomDimension20() {
+        return customDimension20;
+    }
+
+    public void setCustomDimension20(String customDimension20) {
+        this.customDimension20 = customDimension20;
+    }
+}
